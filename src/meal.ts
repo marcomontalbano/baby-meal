d13839b8188aed12ba80193d59a40eae73967472
Marco Montalbano <marcomontalbano.work@gmail.com>
2022-02-13 21:11:04 +0100

    Add UI

--- src/meal.ts
+++ src/meal.ts
@@ -10,10 +10,12 @@ export type Carbohydrate = {
   category: 'crema' | 'pastina'
 }
 
-export const proteins: Protein[] = [
+type NotEmptyArrayOf<T> = [T, ...T[]]
+
+const proteinsA: NotEmptyArrayOf<Protein> = [
   { name: 'Pollo', category: 'carne bianca' },
-  { name: 'Tacchino', category: 'carne bianca' },
   { name: 'Coniglio', category: 'carne bianca' },
+  { name: 'Tacchino', category: 'carne bianca' },
   { name: 'Agnello', category: 'carne bianca' },
   { name: 'Manzo', category: 'carne rossa' },
   { name: 'Vitello', category: 'carne rossa' },
@@ -23,7 +25,38 @@ export const proteins: Protein[] = [
   { name: 'Orata', category: 'fish' },
   { name: 'Platessa', category: 'fish' },
   { name: 'Trota', category: 'fish' },
+]
 
+export const proteins: NotEmptyArrayOf<Protein> = [
+  { name: 'Pollo', category: 'carne bianca' },
+  { name: 'Coniglio', category: 'carne bianca' },
+  { name: 'Manzo', category: 'carne rossa' },
+  { name: 'Nasello', category: 'fish' },
+  
+  { name: 'Tacchino', category: 'carne bianca' },
+  { name: 'Agnello', category: 'carne bianca' },
+  { name: 'Vitello', category: 'carne rossa' },
+  { name: 'Spigola', category: 'fish' },
+  
+  { name: 'Pollo', category: 'carne bianca' },
+  { name: 'Coniglio', category: 'carne bianca' },
+  { name: 'Manzo', category: 'carne rossa' },
+  { name: 'Salmone', category: 'fish' },
+  
+  { name: 'Tacchino', category: 'carne bianca' },
+  { name: 'Agnello', category: 'carne bianca' },
+  { name: 'Vitello', category: 'carne rossa' },
+  { name: 'Orata', category: 'fish' },
+
+  { name: 'Pollo', category: 'carne bianca' },
+  { name: 'Coniglio', category: 'carne bianca' },
+  { name: 'Manzo', category: 'carne rossa' },
+  { name: 'Platessa', category: 'fish' },
+
+  { name: 'Tacchino', category: 'carne bianca' },
+  { name: 'Agnello', category: 'carne bianca' },
+  { name: 'Vitello', category: 'carne rossa' },
+  { name: 'Trota', category: 'fish' },
 ]
 
 export const carbohydrates: Carbohydrate[] = [
--- src/today-meal.ts
+++ src/today-meal.ts
@@ -3,6 +3,8 @@ import { html, css, LitElement } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 
 import { getDinnerMeal, getLunchMeal } from './meal'
+import sun from './images/sunny.svg'
+import moon from './images/full-moon.svg'
 
 /**
  * An example element.
@@ -14,10 +16,57 @@ import { getDinnerMeal, getLunchMeal } from './meal'
 export class TodayMeal extends LitElement {
   static styles = css`
     :host {
-      display: block;
-      border: solid 1px gray;
-      padding: 16px;
-      max-width: 800px;
+      position: absolute;
+      top: 40px;
+      left: 0;
+      width: 100%;
+      height: calc(100% - 40px);
+      background: rgb(161,214,252);
+      background: linear-gradient(135deg, rgba(161,214,252,1) 0%, rgba(65,143,239,1) 50%, rgba(49,53,101,1) 50%, rgba(1,0,17,1) 100%);
+      font-family: 'Montserrat', sans-serif;
+      font-size: 16px;
+    }
+
+    .date {
+      height: 40px;
+      position: absolute;
+      top: -40px;
+      left: 0;
+      width: 100%;
+      line-height: 40px;
+      text-align: center;
+    }
+
+    .day, .night {
+      position: absolute;
+      width: 50%;
+      height: 50%;
+      display: flex;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+      font-size: 18px;
+      min-width: 250px;
+    }
+
+    .day img {
+      width: 150px;
+    }
+
+    .night img {
+      width: 100px;
+    }
+
+    .day {
+      top: 0;
+      left: 0;
+    }
+
+    .night {
+      bottom: 0;
+      right: 0;
+      color: white;
     }
   `
 
@@ -32,14 +81,25 @@ export class TodayMeal extends LitElement {
 
     return html`
         <div>
-          <div>${date.format('dddd DD MMMM YYYY')}</div>
-          <div>${lunchMealProtein.name} - ${lunchMealCarbohydrate.name}</div>
-          <div>${dinnerMealCarbohydrate.name}</div>
+          <div class="date">${date.format('dddd, DD MMMM YYYY')}</div>
+          <div class="day">
+            <div>
+              <img src=${sun} />
+              <div>${lunchMealProtein.name}</div>
+              <div>${lunchMealCarbohydrate.name}</div>
+            </div>
+          </div>
+          <div class="night">
+            <div>
+              <img src=${moon} />
+              <div>${dinnerMealCarbohydrate.name}</div>
+            </div>
+          </div>
         </div>
       `
   }
 
-  render() {
+  renderCalendar() {
     return Array(30).fill(undefined).map((_, i) => {
       return html`
         ${this.renderMeal(dayjs().add(i, 'day'))}
@@ -48,7 +108,7 @@ export class TodayMeal extends LitElement {
     })
   }
 
-  rendder() {
+  render() {
     return this.renderMeal()
   }
 }
